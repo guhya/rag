3002cb3d9164aea13f4c426168cf26a67558e914
Guhya <guhyawijaya@gmail.com>
2024-09-06 17:43:34 +0900

    mysql connect

--- populate_mysql.py
+++ populate_mysql.py
@@ -5,13 +5,23 @@ import mysql.connector
 
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.schema.document import Document
-from get_embedding_function import get_embedding_function
 from langchain_chroma import Chroma
+
+from langchain_community.llms.ollama import Ollama
+from langchain.prompts import ChatPromptTemplate
+from get_embedding_function import get_embedding_function
 
 
 CHROMA_PATH = "chroma_mysql"
 DATA_PATH = "data"
 
+# Setup MySQL and Chroma DB
+mysql_conn = mysql.connector.connect(
+    host="localhost",
+    user="sakila",
+    password="sakila",
+    database="sakila"
+)
 
 def main():
 
@@ -22,6 +32,10 @@ def main():
     if args.reset:
         print("✨ Clearing Database")
         clear_database()
+
+    # generate_descriptions()
+    # if 1==1: 
+    #    return
 
     # Create (or update) the data store.
     documents = load_documents()
@@ -37,25 +51,50 @@ def main():
 
 def load_documents():
 
-    # Setup MySQL and Chroma DB
-    mysql_conn = mysql.connector.connect(
-        host="localhost",
-        user="sakila",
-        password="sakila",
-        database="sakila"
-    )
-
     cursor = mysql_conn.cursor()
     cursor.execute("SELECT film_id, title, description FROM film")
     
     docs = []
     for film_id, title, description in cursor.fetchall():
         str = f"Id: {film_id}\nTitle: {title}\nDescription: {description}\n"
-        doc = Document(page_content=str, metadata={"mysql_id": film_id, "source":"film", "page":"1"})
+        doc = Document(page_content=str, metadata={"mysql_id": film_id, "source":"film"})
         docs.append(doc)
 
     return docs
 
+def generate_descriptions():
+
+    PROMPT_TEMPLATE = """
+    You will generate at least 3 paragraph of a movie description and synopsis.
+    You do not need to style the headings.
+
+    {context}
+
+    Generate the response for the movie title: {question}
+    """
+    model = Ollama(model="llama3.1:8b-instruct-q8_0")
+
+    cursor = mysql_conn.cursor()
+    cursor.execute("SELECT film_id, title FROM film WHERE film_id IN (85, 528)")
+    
+    update_sql = "UPDATE film SET description = %s WHERE film_id = %s"
+    for film_id, title in cursor.fetchall():
+        prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+        prompt = prompt_template.format(context="", question=title)
+
+        response_text = model.invoke(prompt)
+        str = f"Title: {title}\nDescription: {response_text}\n"
+        print(str)
+
+        try:
+            cursor.execute(update_sql, (response_text, film_id))
+            mysql_conn.commit()        
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            mysql_conn.rollback()
+
+    cursor.close()
+    mysql_conn.close()
 
 def split_documents(documents: list[Document]):
     text_splitter = RecursiveCharacterTextSplitter(
@@ -73,28 +112,15 @@ def add_to_chroma(chunks: list[Document]):
         persist_directory=CHROMA_PATH, embedding_function=get_embedding_function()
     )
 
-    # Calculate Page IDs.
+    # Calculate chunk_id metadata.
     chunks_with_ids = calculate_chunk_ids(chunks)
 
     # Add or Update the documents.
     existing_items = db.get(include=[])  # IDs are always included by default
-    print(f"Existing items {existing_items}")
-
     existing_ids = set(existing_items["ids"])
     print(f"Number of existing documents in DB: {len(existing_ids)}")
 
-    # Only add documents that don't exist in the DB.
-    new_chunks = []
-    for chunk in chunks_with_ids:
-        if chunk.metadata["id"] not in existing_ids:
-            new_chunks.append(chunk)
-
-    if len(new_chunks):
-        print(f"👉 Adding new documents: {len(new_chunks)}")
-        new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
-        db.add_documents(new_chunks, ids=new_chunk_ids)
-    else:
-        print("✅ No new documents to add")
+    db.add_documents(chunks_with_ids)
 
 
 def calculate_chunk_ids(chunks):
@@ -104,7 +130,7 @@ def calculate_chunk_ids(chunks):
 
     for chunk in chunks:
         source = chunk.metadata.get("source")
-        page = chunk.metadata.get("page")
+        page = chunk.metadata.get("mysql_id")
         current_page_id = f"{source}:{page}"
 
         # If the page ID is the same as the last one, increment the index.
@@ -118,7 +144,8 @@ def calculate_chunk_ids(chunks):
         last_page_id = current_page_id
 
         # Add it to the page meta-data.
-        chunk.metadata["id"] = chunk_id
+        chunk.metadata["chunk_id"] = chunk_id
+        chunk.metadata["chunk_index"] = current_chunk_index
 
     return chunks
 
--- query_mysql.py
+++ query_mysql.py
@@ -6,30 +6,79 @@ from langchain_community.llms.ollama import Ollama
 from get_embedding_function import get_embedding_function
 
 CHROMA_PATH = "chroma_mysql"
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+Explain why you think your answer is related to the question.
+
+"""
 
 def main():
     # Create CLI.
     parser = argparse.ArgumentParser()
     parser.add_argument("query_text", type=str, help="The query text.")
+    parser.add_argument("--llm", action='store_true', help="Send prompt to LLM.")
+    
     args = parser.parse_args()
     query_text = args.query_text
-    query_rag(query_text)
+
+    query_rag(query_text, args.llm)
 
 
-def query_rag(query_text: str):
+def query_rag(query_text: str, send_to_llm: bool):
     # Prepare the DB.
-    embedding_function = get_embedding_function()
-    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=get_embedding_function())
 
     # Search the DB.
     results = db.similarity_search_with_score(query_text, k=10)
+    doc_sets = set()
     for doc, _score in results:
-        print(f"{_score}\n{doc}\n\n")
+        doc_sets.add(doc.metadata.get("mysql_id", None))
 
 
-    sources = [doc.metadata.get("mysql_id", None) for doc, _score in results]
-    formatted_response = f"Sources: {sources}"
+    # Send to LLM if flag is set
+    if send_to_llm:
+        # Define the metadata criteria
+        metadata_criteria = {"mysql_id": { "$in" : list(doc_sets)}}
+        retriever = db.get(where=metadata_criteria)
+        docs = retriever.get("documents")
+        metadatas = retriever.get("metadatas")
+
+        i = 0
+        combined_docs = [];
+        for doc in metadatas:
+            tmp = metadatas[i]
+            tmp["content"] = docs[i]
+            i += 1
+            combined_docs.append(tmp)
+            
+        combined_docs_sorted = sorted(combined_docs, key=lambda x: (x['mysql_id'], x['chunk_index']))
+
+        mysql_id = -1
+        context_text = ""
+        for doc in combined_docs_sorted:
+            if mysql_id != doc["mysql_id"]:
+                context_text += "\n\n---\n\n"
+            context_text += doc["content"] + " "
+            mysql_id = doc["mysql_id"]
+
+        print(f"Sending prompt to LLM..")
+
+        prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+        prompt = prompt_template.format(context=context_text, question=query_text)
+        print(prompt)
+
+        model = Ollama(model="llama3.1:8b-instruct-q8_0")
+        response_text = model.invoke(prompt)
+
+    formatted_response = f"Response: {response_text}\n\nSources: {doc_sets}"
     print(formatted_response)
+
     return formatted_response
 
 
